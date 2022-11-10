ce74258ca01daa4f24da247d1e87556736be91fb
Kevin DeVore <kdevore10@gmail.com>
2022-11-10 14:30:49 -0500

    Adding finished java to repo

---
+++ Develop/script.js
@@ -0,0 +1,91 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function randomInt(min,max){
+  if(!max){
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 - rand) + rand*max)
+}
+
+
+function getRandomItem(list){
+  return list[randomInt(0, list.length - 1)]
+}
+
+function generatePassword(){
+
+  var userInput = window.prompt("Password Length? 8-128 characters")
+
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)){
+    window.alert("Please enter a number from 8 to 128")
+    return
+  } 
+
+  if (passwordLength < 8 || passwordLength > 128 ){
+    window.alert("Password length must be between 8 and 128 characters")
+    return
+  }
+
+  var userWantsNumbers = window.confirm("Include numbers in password?")
+  var userWantsSymbols = window.confirm("Include symbols in password? ! @ # $ * % ")
+  var userWantsLowercase = window.confirm("Include lowercase letters in password?")
+  var userWantsUppercase = window.confirm("Include uppercase letters in password?")
+
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolList = ["!", "@", "#", "$", "*", "%"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+  var optionsCart = []
+
+
+  if (userWantsNumbers === true){
+    optionsCart.push(numberList)
+  }
+
+  if (userWantsSymbols === true){
+    optionsCart.push(symbolList)
+  }
+
+  if (userWantsLowercase === true){
+    optionsCart.push(lowercaseList)
+  }
+
+  if (userWantsUppercase === true){
+    optionsCart.push(uppercaseList)
+  }
+
+  if (optionsCart.length === 0){
+    alert("Must contain characters")
+    return 
+  } 
+
+  var generatedPassword = ""
+
+  for (var i = 0; i < passwordLength; i++){
+    var randomList = getRandomItem(optionsCart)
+    var randomChar = getRandomItem(randomList)
+    generatedPassword += randomChar 
+  }
+
+  return generatedPassword 
+}
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
